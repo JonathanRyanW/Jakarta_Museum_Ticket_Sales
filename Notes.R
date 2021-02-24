68b330b50e4cad134335c9f9f592a5b598a193b9
Jonathan Ryan <jonathanryan576@gmail.com>
2021-02-24 07:31:09 +0700

    Initial Commit

---
+++ Notes.R
@@ -0,0 +1,19 @@
+""" Ini orang yang bikin data kagak bisa apa ya konsisten dalam hal penamaan
+Di 1 data namanya "Bahari", di data lain namanya "Museum Bahari" Kayak gini kan
+bikin kerjaan data analis jadi susah. Konsisten kenapa sih
+Trus urutan juga dibikin konsisten kenapa, emg sesusah itu ya?
+
+Kenapa data penjualan tiket monas tahun 2015 dan 2016 0?
+
+Pola Taman Arkeologi sama Museum Wayang sama banget. peningkatan di tahun 2013
+dan tahun 2014 abis itu stagnan selama 3 tahun dan meningkat lagi. Ternyata
+sama museum bahari juga sama. Klo museum keramik sama juga tpi ada beda sedikit
+di tahun 2015. Klo museum yang lain kan tahun 2015 stagnan aja, klo keramik
+meningkat juga.
+
+Sebenernya apa sih yang terjadi di tahun 2013 dan 2014, kenapa bisa tiba2
+banyak museum mendapatkan peningkatan pendapatan?
+
+Btw pada saat analisa ini dilakukan (11 Feb 2021), data tahun 2018-2020 belum
+ada di website open data jakarta jadi ya tidak bisa dianalisa.
+"""
